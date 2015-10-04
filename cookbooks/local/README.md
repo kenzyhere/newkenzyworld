be4e8c4a8562cabb41bae6915071fb118cfe83af
kenzy <kenzy@acm.org>
2015-10-04 12:29:43 -0700

    cookbooks added

--- cookbooks/local/README.md
+++
@@ -1,4 +0,0 @@
-# local
-
-TODO: Enter the cookbook description here.
-
--- cookbooks/local/metadata.rb
+++
@@ -1,8 +0,0 @@
-name 'local'
-maintainer 'Ken Young'
-maintainer_email 'kenzy@acm.org'
-license 'apache2'
-description 'Installs/Configures local'
-long_description 'Installs/Configures local'
-version '0.1.0'
-depends 'winbox', '= 0.1.50'
